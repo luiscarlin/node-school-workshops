19ba32c5e96f6d9ced4720c93d7756a6ffd444f6
Luis Carlin <luiscarlin@gmail.com>
2018-01-12 07:56:01 -0500

    every-some

---
+++ src/functional-javascript-workshop/every-some.test.js
@@ -0,0 +1,34 @@
+import subject from './every-some'
+
+describe(subject, () => {
+  let goodUsers
+  let testAllValid
+
+  beforeEach(() => {
+    goodUsers = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 }
+    ]
+    testAllValid = subject(goodUsers)
+  })
+
+  test('when in the list', () => {
+    let result = testAllValid([
+      { id: 2 },
+      { id: 1 }
+    ])
+
+    expect(result).toBe(true)
+  })
+
+  test('when not in the good list', () => {
+    let result = testAllValid([
+      { id: 2 },
+      { id: 4 },
+      { id: 1 }
+    ])
+
+    expect(result).toBe(false)
+  })
+})
