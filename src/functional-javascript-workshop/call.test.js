932632ef4ce43ec1dc5b884219fcfee2c4c1619d
Luis Carlin <luiscarlin@gmail.com>
2018-01-15 06:43:09 -0500

    accept variable number of args

--- src/functional-javascript-workshop/call.test.js
+++ src/functional-javascript-workshop/call.test.js
@@ -1,9 +1,24 @@
 import subject from './call'
 
-it(subject, () => {
-  let notDuck = Object.create({quack: true})
-  let duck = {quack: true}
-  let output = subject(duck, notDuck)
+describe(subject, () => {
+  it('takes a variable number of arguments without throwing error', () => {
+    subject({boolean: true}, {hello: 'value'}, {num: 2}, {someprop: 'somevalue'}, {}, {})
+    subject()
+  })
 
-  expect(output).toBe(1)
+  xit('returns number of objects passed as arguments that contain "quack" property', () => {
+    let noDuck = Object.create({quack: true})
+    let duck = {quack: true}
+    let output = subject(duck, noDuck)
+
+    expect(output).toBe(1)
+  })
+
+  xit('returns 0 if none of the objects being passed as arguments contain a "quack" property', () => {
+    let noDuck = Object.create({quack: true})
+    let noDuckEither = {someFunc: (a) => a, someProp: 5}
+    let output = subject(noDuck, noDuckEither)
+
+    expect(output).toBe(0)
+  })
 })
