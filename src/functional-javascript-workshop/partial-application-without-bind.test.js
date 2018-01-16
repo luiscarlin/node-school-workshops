c18dcf7598fc94005cd6665f73fb29b2e7d82dd3
Luis Carlin <luiscarlin@gmail.com>
2018-01-16 07:27:38 -0500

    mock console.log and make sure it's called

--- src/functional-javascript-workshop/partial-application-without-bind.test.js
+++ src/functional-javascript-workshop/partial-application-without-bind.test.js
@@ -1,7 +1,16 @@
 import subject from './partial-application-without-bind'
 
-it(subject, () => {
-  var info = subject('INFO:')
-  info('this is an info message')
-  // INFO: this is an info message
+describe(subject, () => {
+  it('returns a new function when called', () => {
+    expect(subject('INFO:')).not.toBeNull()
+  })
+
+  it('prints message to console.log', () => {
+    console.log = jest.fn()
+
+    let info = subject('INFO:')
+
+    info('this is an info message')
+    expect(console.log).toHaveBeenCalledWith('INFO:', 'this is an info message')
+  })
 })
