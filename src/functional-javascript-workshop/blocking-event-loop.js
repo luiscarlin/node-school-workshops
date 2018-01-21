2371339d1ce9e99b164f58c79561d1f5ca3b9385
Luis Carlin <luiscarlin@gmail.com>
2018-01-21 10:16:07 -0500

    Explanation on blocking

--- src/functional-javascript-workshop/blocking-event-loop.js
+++ src/functional-javascript-workshop/blocking-event-loop.js
@@ -24,21 +24,29 @@
 
 //   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Timers
 
-// ## Boilerplate
+/*
+  PROBLEM:
+  Javascript has 1 event queue. Events are added to that queue and run in a loop. One by one by the Javascript engine.
+  Synchronous "blocking" code (i.e. execution waits until prev process finishes) blocks the event loop. That means that timers, IO handlers can't fire
+  while the blocking code is running.
+  Since this is a recursive function that could run for a long time, we want to force it release control of the event loop often.
+
+  SOLUTION:
+  Use setTimeout(function, milliseconds). This way the function will be placed in the event queue only after # milliseconds specified. During the timeout,
+  other events will be run in the event loop.
+*/
 
 function repeat (operation, num) {
-  // modify this so it can be interrupted
   if (num <= 0) return
 
+  // perform the operation
   operation()
 
-  // release control every 10 or so
-  // iterations.
-  // 10 is arbitrary.
+  // every 10 iterations, take a break and allow other events to run
   if (num % 10 === 0) {
     setTimeout(function () {
       repeat(operation, --num)
-    })
+    }, 100)
   } else {
     repeat(operation, --num)
   }
